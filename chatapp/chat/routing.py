6d1bf3e5bd045eac46ce641a058a5f8b6b83dd49
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-12-30 10:05:06 +0500

    Add files via upload

---
+++ chatapp/chat/routing.py
@@ -0,0 +1,10 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import re_path
+from chatapp.chat.consumers import ChatConsumer
+
+#
+# from chat.consumers import ChatConsumer
+
+websocket_urlpatterns = [
+ re_path(r'chat/', ChatConsumer.as_asgi()),
+]
