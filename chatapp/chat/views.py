c9bc0a207389300bd7ecbfc9b86156f0e1a2336b
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-12-30 10:26:14 +0500

    Delete chatapp directory

--- chatapp/chat/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import generics
-from .models import Message
-from .serializers import MessageSerializer
-
-class MessageList(generics.ListCreateAPIView):
-    queryset = Message.objects.all()
-    serializer_class = MessageSerializer
-    ordering = ('-timestamp',)
